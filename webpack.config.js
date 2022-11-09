67d0851f9b25efa313d1b9f9596d67bd1cf5776d
Devesh Kumar <devesh2027@gmail.com>
2022-11-10 00:13:31 +0530

    Added Bundle Generation for packages and browsers

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require("path");
+
+module.exports = {
+	entry: "./dist/index.js",
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "index.js",
+		libraryExport: "default",
+		library: "waterfall",
+		libraryTarget: "umd",
+	},
+};
