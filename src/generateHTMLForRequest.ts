136cea69789f06127f1cb0cfa2a8f0470b55e753
Devesh Kumar <devesh2027@gmail.com>
2022-11-12 19:23:17 +0530

    fix: Left attribute of waterfall request

--- src/generateHTMLForRequest.ts
+++ src/generateHTMLForRequest.ts
@@ -10,7 +10,9 @@ const generateHTMLForRequest = (
 			(endBound.getTime() - startBound.getTime())) *
 		100;
 	const requestLeftOffset =
-		(request.startedAt.getTime() - startBound.getTime()) / 100;
+		((request.startedAt.getTime() - startBound.getTime()) /
+			(endBound.getTime() - startBound.getTime())) *
+		100;
 	return `
 	<div class="waterfall-request-row" title="${request.url}">
 		<div class="waterfall-request" style="width: ${requestWidth}%; left:${requestLeftOffset}%"></div>
